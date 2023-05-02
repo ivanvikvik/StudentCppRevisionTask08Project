4912d346f1803e7830755e9a4f2c1d4b88d5821c
Victor Ivanchenko <ivanvikvik@gmail.com>
2023-05-02 17:40:32 +0300

    Update tests.h

--- StudentCppRevisionTask08Project/tests.h
+++ StudentCppRevisionTask08Project/tests.h
@@ -16,6 +16,11 @@ void testingTask01() {
 		&& task01(8, 7, 7, 7) == 3
 		&& task01(7, 8, 9, 7) == 2
 		&& task01(7, 9, 8, 7) == 2
+		&& task01(7, 8, 8, 7) == 2
+		&& task01(7, 8, 7, 8) == 2
+		&& task01(7, 7, 8, 8) == 2
+		&& task01(8, 7, 7, 8) == 2		
+		&& task01(8, 7, 8, 7) == 2		
 		&& task01(8, 9, 7, 7) == 2;
 
 	cout << "Task 01 [The same Numbers]" << (result ? RIGHT : WRONG) << endl;
